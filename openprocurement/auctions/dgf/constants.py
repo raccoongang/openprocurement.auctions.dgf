83a5ca1984479fb626bb2dc103700b7e540ff58f
Anton Korobko <anton.korobko@raccoongang.com>
2017-09-14 11:25:50 +0300

    review branch start

--- openprocurement/auctions/dgf/constants.py
+++ openprocurement/auctions/dgf/constants.py
@@ -2,6 +2,6 @@
 from datetime import datetime, timedelta, time, date
 from openprocurement.api.models import (TZ)
 
-
+MINIMAL_PERIOD_FROM_EQUIRY_END = timedelta(days=5)
 MINIMAL_EXPOSITION_PERIOD = timedelta(days=7)
 MINIMAL_EXPOSITION_REQUIRED_FROM = datetime(2017, 8, 10, tzinfo=TZ)
